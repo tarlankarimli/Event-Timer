313d05cef18a6a9b875745cac435ad79fc096273
Tarlan A. Karimli <KarimliT@kapitalho.az>
2020-11-28 19:14:39 +0400

    finished, not responsive

--- js/index.js
+++ js/index.js
@@ -6,34 +6,38 @@ const timeValue = document.querySelector('#time');
 const eventBtn = document.querySelector('.btn-add-event');
 const eventsList = document.querySelector('.events');
 const formHeading = document.querySelector('.form-heading');
-const countDown = document.querySelector('.event-time-bg');
+const showYears = document.querySelector('.years');
 const showMonths = document.querySelector('.months');
 const showDays = document.querySelector('.days');
+const formModal = document.querySelector('.form-container');
 const showHours = document.querySelector('.hours');
 const showMinutes = document.querySelector('.minutes');
 const showSeconds = document.querySelector('.seconds');
+const countDown = document.querySelector('.event-time-bg');
 const eventItemTitle = document.querySelector('.event-item-title');
 const eventDescription = document.querySelector('.event-desc');
 const removeItem = document.querySelector('.remove-btn');
+const menuBar = document.querySelector('.menu-bar');
+const eventTimeModal = document.querySelector('.event-item-time');
 let evenData;
-let currentIndex = 0;
+let years;
 let months;
 let days;
 let hours;
 let minutes;
 let seconds;
 const calendar = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
 // check is data existed
-//  localStorage.removeItem('eventStorage')
-window.onload = ()=> {
+window.onload = checkData = ()=> {
     if (localStorage.getItem('eventStorage') == null) {       
         eventData = [];
     } else {
         eventData = JSON.parse(localStorage.getItem('eventStorage'));
-        currentIndex = eventData.length      
-        showEvents();        
+        showEvents();    
     }
 }
+
 //  check item is validate
 const checkValidate = () => {
     if ((titleValue.value && dateValue.value && timeValue.value) == '') {
@@ -44,6 +48,7 @@ const checkValidate = () => {
     formHeading.textContent = 'Create new event';
     formHeading.style.color = '#fff';    
 }
+
 // remove event item
 removeItem.addEventListener('dragover', (e)=>{e.preventDefault()})
 removeItem.addEventListener('drop', (e) => {
@@ -53,6 +58,14 @@ removeItem.addEventListener('drop', (e) => {
     localStorage.setItem('eventStorage', JSON.stringify(eventData));
     showEvents();
 })
+
+// remove all items
+removeItem.addEventListener('click', () => {
+    localStorage.removeItem('eventStorage');
+    eventsList.textContent= '';
+    checkData()
+})
+
 // add event
 eventBtn.addEventListener('click', ()=> {
     if(checkValidate()==false) {
@@ -63,14 +76,11 @@ eventBtn.addEventListener('click', ()=> {
 })
 
 const addEvent = () => {
-    currentIndex = eventData.length
     let eventItem = {
-        id: currentIndex,
         title: titleValue.value,
         description: descValue.value,
         date: `${dateValue.value},${timeValue.value}`
     }
-    currentIndex++;
     eventData.push(eventItem)
     localStorage.setItem('eventStorage', JSON.stringify(eventData));
 
@@ -79,17 +89,19 @@ const addEvent = () => {
     dateValue.value = '';
     timeValue.value = '';
 }
+
 //  show all events
 const showEvents = () => {
-    eventsList.textContent= '';
-    eventData.map((data) => {
+    eventsList.textContent= '';    
+    eventData.map((data, index) => {
         let inputDate = new Date(data.date);
-        addEventItem(data.id, data.title, inputDate.getDate(), calendar[inputDate.getMonth()])
+        addEventItem(index, data.title, inputDate.getDate(), calendar[inputDate.getMonth()])
     })
 }
+
 // creating new event item elements and setting attributes
 const addEventItem = (id, title, date, month) => {
-    const eventMonth = document.createElement('span');
+    const eventMonth = document.createElement('div');
     eventMonth.setAttribute('class','event-month');
 
     const eventDay = document.createElement('div');
@@ -109,8 +121,10 @@ const addEventItem = (id, title, date, month) => {
     eventItem.addEventListener('dragstart', (e)=>(e.dataTransfer.setData("item", e.target.id)));
 
     eventItem.addEventListener('click', ()=> {
-        countDown.className = 'event-time-bg time-animation';
-        countDown.style.top = 0;        
+        countDown.className = 'event-time-bg show-timer';
+        removeItem.style.display = 'none';
+
+        countDown.style.top = 0;     
             setInterval(() => {
             convertInterval(id);
            }, 1000);
@@ -133,16 +147,21 @@ const convertInterval = (id) => {
     let customDate = new Date(new Date(eventData[id].date).getTime())
     let interval = customDate - currentDate;
     
-    months = Math.floor((interval % (1000 * 60 * 60 * 24 * 30 * 12)) / (1000 * 60 * 60 *24 * 30))
-    days = Math.floor((interval % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 *24))
-    hours = Math.floor((interval % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    minutes = Math.floor((interval % (1000 * 60 * 60)) / (1000 * 60));
-    seconds = Math.floor((interval % (1000 * 60)) / 1000);
+    if(interval>0){
+        years = Math.floor((interval % (1000 * 60 * 60 * 24 * 30 * 12 *1000)) / (1000 * 60 * 60 *24 * 30 *12))
+        months = Math.floor((interval % (1000 * 60 * 60 * 24 * 30 * 12)) / (1000 * 60 * 60 *24 * 30))
+        days = Math.floor((interval % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 *24))
+        hours = Math.floor((interval % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        minutes = Math.floor((interval % (1000 * 60 * 60)) / (1000 * 60));
+        seconds = Math.floor((interval % (1000 * 60)) / 1000);
+    }
 
-    getItemInfo(days, hours, minutes, seconds, months, id);
+    getItemInfo(days, hours, minutes, seconds, months, years, id);
 }
+
 // writing countdown and item info to display countdown section
-const getItemInfo = (days, hours, minutes, seconds,months, id) => {
+const getItemInfo = (days=0, hours=0, minutes=0, seconds=0,months=0, years=0,id) => {
+    showYears.textContent = years;
     showMonths.textContent = months;
     showDays.textContent = days;
     showHours.textContent = hours;
@@ -152,4 +171,11 @@ const getItemInfo = (days, hours, minutes, seconds,months, id) => {
     eventItemTitle.textContent = eventData[id].title;
     eventDescription.textContent = eventData[id].description;    
 }
+
+// close countdown modal
+countDown.addEventListener('click', (e)=> {
+    if (e.target == countDown) {
+        location.reload();
+    }
+})
 
